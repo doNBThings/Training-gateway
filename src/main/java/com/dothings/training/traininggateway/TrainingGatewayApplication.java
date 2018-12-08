c35258a66fd4b97695a0dc436c0e2e60e370e470
mackson <1430665712@qq.com>
2018-12-08 15:15:27 +0800

    first commit

---
+++ src/main/java/com/dothings/training/traininggateway/TrainingGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.dothings.training.traininggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class TrainingGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingGatewayApplication.class, args);
+	}
+}
